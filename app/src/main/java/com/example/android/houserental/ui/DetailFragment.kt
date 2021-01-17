6d19ac5e1b6df6b02b3303edaeee5e5d469ae61a
AxelJurczuk <61692326+AxelJurczuk@users.noreply.github.com>
2021-01-17 13:06:03 -0300

    code associated with map and location permissions

--- app/src/main/java/com/example/android/houserental/ui/DetailFragment.kt
+++ app/src/main/java/com/example/android/houserental/ui/DetailFragment.kt
@@ -1,6 +1,7 @@
 package com.example.android.houserental.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -9,13 +10,17 @@ import androidx.navigation.fragment.navArgs
 import com.example.android.houserental.R
 import com.example.android.houserental.databinding.FragmentDetailBinding
 import com.example.android.houserental.domain.model.House
+import com.google.android.gms.maps.*
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.squareup.picasso.Picasso
 
-class DetailFragment : Fragment() {
+class DetailFragment : Fragment(), OnMapReadyCallback {
 
     private lateinit var binding: FragmentDetailBinding
     private val args:DetailFragmentArgs by navArgs()
     private lateinit var house:House
+    private lateinit var map:GoogleMap
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,6 +30,7 @@ class DetailFragment : Fragment() {
         binding= FragmentDetailBinding.inflate(layoutInflater)
 
         house = args.houseDetail
+        createMapFragment()
         return binding.root
     }
 
@@ -41,5 +47,29 @@ class DetailFragment : Fragment() {
         binding.tvSize.text= house.size.toString()
         binding.tvPrice.text= house.price.toString()
         binding.tvDescription.text= house.description
+
+    }
+    private fun createMapFragment() {
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap?) {
+        if (googleMap != null) {
+            map = googleMap
+            createMarker()
+        }
+    }
+
+    private fun createMarker() {
+        val coordinates = LatLng(house.latitude, house.longitude)
+        val marker = MarkerOptions().position(coordinates)
+        Log.d("coordinates", coordinates.toString())
+        map.addMarker(marker)
+        map.animateCamera(
+            CameraUpdateFactory.newLatLngZoom(coordinates, 15f),
+            4000,
+            null
+        )
     }
 }
--- app/src/main/java/com/example/android/houserental/ui/OverviewFragment.kt
+++ app/src/main/java/com/example/android/houserental/ui/OverviewFragment.kt
@@ -1,5 +1,7 @@
 package com.example.android.houserental.ui
 
+import android.Manifest
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -15,9 +17,12 @@ import com.example.android.houserental.data.Result
 import com.example.android.houserental.databinding.FragmentOverviewBinding
 import com.example.android.houserental.ui.adapter.ItemHouseAdapter
 import com.example.android.houserental.domain.model.House
+import com.example.android.houserental.location.TrackingUtility
 import com.example.android.houserental.viewmodel.HouseViewModel
+import pub.devrel.easypermissions.AppSettingsDialog
+import pub.devrel.easypermissions.EasyPermissions
 
-class OverviewFragment : Fragment(), ItemHouseAdapter.OnItemClick {
+class OverviewFragment : Fragment(), ItemHouseAdapter.OnItemClick,EasyPermissions.PermissionCallbacks {
 
     private lateinit var binding: FragmentOverviewBinding
     private val viewModel: HouseViewModel by viewModels()
@@ -67,7 +72,7 @@ class OverviewFragment : Fragment(), ItemHouseAdapter.OnItemClick {
 
         val recyclerView = binding.recyclerView
         val searchView = binding.searchView
-
+        requestPermissions()
         adapter = ItemHouseAdapter(requireContext(), this)
         recyclerView.adapter = adapter
         recyclerView.setHasFixedSize(true)
@@ -87,6 +92,49 @@ class OverviewFragment : Fragment(), ItemHouseAdapter.OnItemClick {
         })
 
     }
+    private fun requestPermissions() {
+        if(TrackingUtility.hasLocationPermissions(requireContext())) {
+            return
+        }
+        if(Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
+            EasyPermissions.requestPermissions(
+                this,
+                "You need to accept location permissions to use this app.",
+                TrackingUtility.REQUEST_PERMISSION_CODE,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            )
+        } else {
+            EasyPermissions.requestPermissions(
+                this,
+                "You need to accept location permissions to use this app.",
+                TrackingUtility.REQUEST_PERMISSION_CODE,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_BACKGROUND_LOCATION
+            )
+        }
+    }
+
+    override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>) {
+        if(EasyPermissions.somePermissionPermanentlyDenied(this, perms)) {
+            AppSettingsDialog.Builder(this).build().show()
+        } else {
+            requestPermissions()
+        }
+    }
+
+    override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>) {}
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
+    }
+
 
     override fun onItemClickListener(position: Int) {
         val house = adapter.houseList[position]
